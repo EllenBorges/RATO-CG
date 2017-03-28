95818425fb729d2c65b2d0a99b6b32b4e84c27a5
EllenBorges <ellen.pboliveira@gmail.com>
2017-03-28 16:32:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <math.h>
-#define PI 3.141592654
+
 #include <GL/glut.h>
 
+/*--------------------Constantes-----------------------------------*/
+#define PI 3.141592654
+#define MAX_NO_TEXTURES 5
 /*------------------Variaveis Globais------------------------------*/
-float X = 0, Y = 0, w = 500, h = 500;
+float X = 0, Y = 0, w = 500, h = 500, CentroX_Rato = 0,CentroY_Rato = 0;
 int tamanho = 25;
+float RatoX[200], RatoY[200];
+float Centro_Rato = 0;
 
 /*------------------Estrutura da Lista------------------------------*/
 typedef struct no_lista{
@@ -147,21 +152,35 @@ void inicializa_Tela(int w, int h, int x, int y, char nome_janela[]){
 void cor_Corrente(char nome_cor[]){
 
     if(strcmp (nome_cor, "branco") == 0)
-        glColor3f(255,255,255);
+        glColor3f(1,1,1);
     else if(strcmp (nome_cor, "preto") == 0)
         glColor3f(0,0,0);
     else if(strcmp (nome_cor, "vermelho") == 0)
-        glColor3f(255,0,0);
+        glColor3f(1,0,0);
     else if(strcmp (nome_cor, "azul") == 0)
-        glColor3f(0,0,255);
+        glColor3f(0,0,1);
     else if(strcmp (nome_cor, "verde") == 0)
-        glColor3f(0,255,0);
+        glColor3f(0,1,0);
     else if(strcmp (nome_cor, "amarelo") == 0)
-        glColor3f(255,255,0);
+        glColor3f(1,1,0);
     else if(strcmp (nome_cor, "magenta") == 0)
-        glColor3f(255,0,255);
+        glColor3f(1,0,1);
     else if(strcmp (nome_cor, "ciano") == 0)
-        glColor3f(0,255,255);
+        glColor3f(0,1,1);
+    else if(strcmp (nome_cor, "cinza") == 0)
+        glColor3f(0.86,0.86,0.86);
+     else if(strcmp (nome_cor, "rosa claro") == 0)
+        glColor3f(1,0.71,0.76);
+    else if(strcmp (nome_cor, "salmao claro") == 0)
+        glColor3f(1,0.63,0.48);
+    else if(strcmp (nome_cor, "salmao") == 0)
+        glColor3f(0.98,0.5,0.45);
+    else if(strcmp (nome_cor, "cinza ardosia escuro") == 0)
+        glColor3f(0.18,0.31,0.31);
+    else if(strcmp (nome_cor, "ouro") == 0)
+        glColor3f(1,0.84,0);
+
+
 
 
 
@@ -171,14 +190,18 @@ void cor_Corrente(char nome_cor[]){
 /*---------------------- FUNCOES DE TRANSFORMAÇÃO---------------------------*/
 
 
-void rotaciona_Rato(){
-
+void rotaciona_Rato(float x, float y){
+    float xlinha, ylinha,angulo_teta,x_r=0,y_r=0;
+    xlinha = (x_r + (x - x_r)*cos(angulo_teta))- ((y - y_r)*sin(angulo_teta));
+    ylinha = (y_r + (x - x_r)*sin(angulo_teta)) + ((y - y_r)*cos(angulo_teta));
 }
 
 void reflexao_Rato(){
 
+
 }
 void translada_Rato(){
+
 
 }
 
@@ -188,9 +211,12 @@ void Escala_Rato(){
 
 
 }
+/*----------------------FUNÇÕES DE TEXTURA-----------------------------*/
+
+void Textura(){
 
 
-
+}
 /*---------------------- FUNCOES DE INTERACAO---------------------------*/
 
 
@@ -253,7 +279,7 @@ void MouseInt (int botao, int estado, int x, int y)
 
 void desenha_Queijo(){
 
-    cor_Corrente("amarelo");
+    cor_Corrente("ouro");
     glPointSize(20.0);
     glBegin(GL_POINTS);
         glVertex2f(X, Y);
@@ -266,32 +292,150 @@ void desenha_Rato(){
     float angulo,x,y;
 
     cor_Corrente("preto");
-
-    /*desenha corpo */
+/*
+    /*desenha corpo*/
     glBegin(GL_POLYGON);
 
         for (i=0; i<=50 ;i++){
             angulo = 2 * PI * i / 50.0;
-            x=0.1*cos(angulo);
-            y=0.2*sin(angulo);
+            x=0.22*cos(angulo);
+            y=0.22*sin(angulo);
+
             glVertex2f(x,y);
+
         }
+
     glEnd();
 
-    /*desenha cabeça */
+    /*desenha cabeça*/
+
+    glBegin(GL_POLYGON);
+        cor_Corrente("preto");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.15*cos(angulo);
+            y=0.17*sin(angulo);
+            glVertex2f(x,y-0.17);
+        }
+
+    glEnd();
+
+    /*desenha orelha esquerda*/
 
     glBegin(GL_POLYGON);
 
+        cor_Corrente("cinza ardosia escuro");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.1*cos(angulo);
+            y=0.1*sin(angulo);
+            glVertex2f(x-0.1,y-0.1);
+        }
+
+
+        cor_Corrente("salmao claro");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.07*cos(angulo);
+            y=0.07*sin(angulo);
+            glVertex2f(x-0.1,y-0.12);
+        }
+    glEnd();
+
+    /*desenha orelha direita*/
+
+    glBegin(GL_POLYGON);
+        cor_Corrente("cinza ardosia escuro");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.1*cos(angulo);
+            y=0.1*sin(angulo);
+            glVertex2f(x+0.13,y-0.1);
+        }
+        cor_Corrente("salmao claro");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.07*cos(angulo);
+            y=0.07*sin(angulo);
+            glVertex2f(x+0.13,y-0.12);
+        }
+
+    glEnd();
+
+    /*desenha nariz*/
+
+    glBegin(GL_POLYGON);
+        cor_Corrente("salmao");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.025*cos(angulo);
+            y=0.025*sin(angulo);
+            glVertex2f(x,y-0.32);
+        }
+
+    glEnd();
+    /*desenha olho direito*/
+
+    glBegin(GL_POLYGON);
+        cor_Corrente("branco");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.025*cos(angulo);
+            y=0.025*sin(angulo);
+            glVertex2f(x+0.09,y-0.22);
+        }
+        cor_Corrente("preto");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.015*cos(angulo);
+            y=0.015*sin(angulo);
+            glVertex2f(x+0.085,y-0.212);
+        }
+
+    glEnd();
+    /*desenha olho esquerdoa*/
+
+    glBegin(GL_POLYGON);
+        cor_Corrente("branco");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.025*cos(angulo);
+            y=0.025*sin(angulo);
+            glVertex2f(x-0.09,y-0.22);
+        }
+        cor_Corrente("preto");
+        for (i=0; i<=50 ;i++){
+            angulo = 2 * PI * i / 50.0;
+            x=0.015*cos(angulo);
+            y=0.015*sin(angulo);
+            glVertex2f(x-0.085,y-0.212);
+        }
 
     glEnd();
 
     /*desenha rabo */
+    cor_Corrente("preto");
 
-    glBegin(GL_POLYGON);
+    glLineWidth(5.0);
+    float P0X=0,P0Y=0.2,
+    P1X=-0.3,P1Y=0.4,
+    P2X=0.3,P2Y=0.4,
+    P3X=0,P3Y=0.6,t;
 
+    glBegin(GL_LINE_STRIP);
 
+        for (t=0; t<=1 ;t=t+0.01){
+
+            /*Curva de Bezier*/
+
+            y =(pow((1-t),3) * P0Y) +(3 * t * pow((1-t),2)*P1Y) + (3 * pow(t,2)* (1-t)*P2Y) + (pow(t,3)*P3Y);
+            x =(pow((1-t),3) * P0X) +(3 * t * pow((1-t),2)*P1X) + (3 * pow(t,2)* (1-t)*P2X) + (pow(t,3)*P3X);
+
+            glVertex2f(x,y);    
+
+        }
+    
     glEnd();
-
 
     glFlush();
 
@@ -308,7 +452,7 @@ int main(int argc, char *argv[]){
     glutKeyboardFunc(keyboard);
     glutMouseFunc(MouseInt);
     glutMainLoop();
-
+    free(listaQueijo);
 
 return 0;
 }
